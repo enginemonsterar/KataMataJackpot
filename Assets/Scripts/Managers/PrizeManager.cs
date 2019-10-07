b544368d67e91dd0cec8fd64cf2584e8144d6e84
Januar Elsan <januarelsan@gmail.com>
2019-10-07 16:51:39 +0700

    Finish Random Result and Json

--- Assets/Scripts/Managers/PrizeManager.cs
+++ Assets/Scripts/Managers/PrizeManager.cs
@@ -2,42 +2,180 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using MonsterAR.Utility;
 
 
 [System.Serializable]
-public class PrizeManager : MonoBehaviour
+public class PrizeManager : Singleton<PrizeManager>
 {
-    
+    [SerializeField] private Sprite[] prizeSprites;
     private bool canGivePrize;
     private WinPrizeData[] winPrizeDatas;
     
 
     void Start(){
-        CreateWinPrizeData(5);
-        // CheckCanGivePrize();
+        // CreateWinPrizeData(0);
+        // CreateWinPrizeData(1);
+        // CreateWinPrizeData(2);
+        // CreateWinPrizeData(3);
+        // ReadAllJsonData();
+        // Debug.Log(CheckCanGivePrizeByTime());
+        // Debug.Log("Can Give Prize by Index 0: " + CheckCanGivePrizeByPrizeIndex(0));
+        // Debug.Log("Can Give Prize by Index 1: " + CheckCanGivePrizeByPrizeIndex(1));
+        // Debug.Log("Can Give Prize by Index 2: " + CheckCanGivePrizeByPrizeIndex(2));
+        // Debug.Log("Can Give Prize by Index 3: " + CheckCanGivePrizeByPrizeIndex(3));
     }
 
-    private void CheckCanGivePrize(){
+    private void ReadAllJsonData(){
         string jsonFile = File.ReadAllText(Application.dataPath + "/winPriteDataFile.json");        
-        WinPrizeData tempNowDate = JsonUtility.FromJson<WinPrizeData>(jsonFile);
-        Debug.Log("Day: " + tempNowDate.Day);
-        Debug.Log("Hour: " + tempNowDate.Hour);
-        Debug.Log("Minute: " + tempNowDate.Minute);
-        Debug.Log("Prize Index: " + tempNowDate.PrizeIndex);
+        WinPrizeData[] tempData = JsonHelper.FromJson<WinPrizeData>(jsonFile);
+        
+        if(tempData != null){
+            for (int i = 0; i < tempData.Length; i++)
+            {
+                Debug.Log("===============  "+ i +"  =============");
+                Debug.Log("Day " + i + ": " +tempData[i].Day);
+                Debug.Log("Hour "  + i + ": " + tempData[i].Hour);
+                Debug.Log("Minute " + i + ": " + tempData[i].Minute);
+                Debug.Log("Prize Index " + i + ": " + tempData[i].PrizeIndex);
+            }
+            
+        }else
+        {
+            Debug.LogWarning("No Data on JsonFile");               
+        }
     }
 
-    private WinPrizeData CreateWinPrizeData(int prizeIndex){
-        WinPrizeData winPrizeData = new WinPrizeData();
-        winPrizeData.Day = System.DateTime.Now.Day;
-        winPrizeData.Hour = System.DateTime.Now.Hour;
-        winPrizeData.Minute = System.DateTime.Now.Minute;        
-        winPrizeData.PrizeIndex = prizeIndex;
+    public int GetJsonDataLenght(){
+        string jsonFile = File.ReadAllText(Application.dataPath + "/winPriteDataFile.json");        
+        WinPrizeData[] tempData = JsonHelper.FromJson<WinPrizeData>(jsonFile);
+        if(tempData != null)
+            return tempData.Length;
+        else
+        {
+            return 0;
+        }
+    }
 
-        string jsonData = JsonUtility.ToJson(winPrizeData);
+    public void CheckResult(){
+        bool anySlotAvailable = false;
+        if(SlotManager.Instance.GetSlotResultA() == SlotManager.Instance.GetSlotResultB() && SlotManager.Instance.GetSlotResultB() == SlotManager.Instance.GetSlotResultC()){                
+                        
+            if(PrizeManager.Instance.GetJsonDataLenght() == 0){
+                PrizeManager.Instance.CreateWinPrizeData(SlotManager.Instance.GetSlotResultA());
+            }else{
+                if(CheckCanGivePrizeByPrizeIndex(SlotManager.Instance.GetSlotResultA())){                        
+                    CreateWinPrizeData(SlotManager.Instance.GetSlotResultA());
+                }else{
+                    Debug.LogWarning("Prize Index " +  SlotManager.Instance.GetSlotResultA() + " is Empty" );
+                    for (int i = 0; i < prizeSprites.Length; i++)
+                    {
+                        if(CheckCanGivePrizeByPrizeIndex(i)){
+                            CreateWinPrizeData(i);
+                            SlotManager.Instance.SetSlotResults(i);
+                            anySlotAvailable = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if(!anySlotAvailable){
+                SlotManager.Instance.EmptyPrizeResult();
+            }
+
+            SlotManager.Instance.StopRollingSlots();
+        }else
+        {            
+            SlotManager.Instance.StopRollingSlots();
+            //For Auto Rolling
+            // SlotManager.Instance.GoRolling();
+        }
+
+    }
+
+    public bool CheckCanGivePrizeByTime(){
+        string jsonFile = File.ReadAllText(Application.dataPath + "/winPriteDataFile.json");        
+        WinPrizeData[] tempData = JsonHelper.FromJson<WinPrizeData>(jsonFile);
+                
+        if(tempData != null){
+            int lastDataIndex = tempData.Length - 1;            
+            
+            if((System.DateTime.Now.Hour - tempData[lastDataIndex].Hour) >= 2){
+                return true;
+            }else{
+                return false;
+            }
+            
+        }else
+        {
+            Debug.LogWarning("No Data on JsonFile");
+            return true;
+        }
+    }
+
+    public bool CheckCanGivePrizeByPrizeIndex(int prizeIndex){
+        string jsonFile = File.ReadAllText(Application.dataPath + "/winPriteDataFile.json");        
+        WinPrizeData[] tempData = JsonHelper.FromJson<WinPrizeData>(jsonFile);
+                
+        if(tempData != null){
+            int lastDataIndex = tempData.Length - 1;            
+            bool canGive = true;
+            for (int i = 0; i < tempData.Length; i++)
+            {
+                if(tempData[i].PrizeIndex == prizeIndex)
+                    canGive = false;                
+            }            
+
+            return canGive;
+            
+        }else
+        {
+            Debug.LogWarning("No Data on JsonFile");
+            return true;
+        }
+    }
+
+    public void CreateWinPrizeData(int prizeIndex){
+
+        //Get All item on Json
+        string jsonFile = File.ReadAllText(Application.dataPath + "/winPriteDataFile.json");        
+        WinPrizeData[] tempData = JsonHelper.FromJson<WinPrizeData>(jsonFile);
+
+
+        //Create array with new data slot                
+        WinPrizeData[] winPrizeData;
+        int newDataIndex;
+        if(tempData != null){
+            winPrizeData = new WinPrizeData[tempData.Length + 1];
+            newDataIndex = tempData.Length;
+            // Debug.Log(winPrizeData.Length);
+            for (int i = 0; i < tempData.Length; i++)
+            {
+                winPrizeData[i] = new WinPrizeData();
+                winPrizeData[i].Day = tempData[i].Day;
+                winPrizeData[i].Hour = tempData[i].Hour;
+                winPrizeData[i].Minute = tempData[i].Minute;
+                winPrizeData[i].PrizeIndex = tempData[i].PrizeIndex;
+            }
+        }
+        else{
+            newDataIndex = 0;
+            winPrizeData = new WinPrizeData[1];
+        }
+        
+        
+        winPrizeData[newDataIndex] = new WinPrizeData();
+        winPrizeData[newDataIndex].Day = System.DateTime.Now.Day;
+        winPrizeData[newDataIndex].Hour = System.DateTime.Now.Hour;
+        winPrizeData[newDataIndex].Minute = System.DateTime.Now.Minute;        
+        winPrizeData[newDataIndex].PrizeIndex = prizeIndex ;
+
+        string jsonData = JsonHelper.ToJson(winPrizeData, true);
         // Debug.Log("Save WinData JSON: " + jsonData);
 
         File.WriteAllText(Application.dataPath + "/winPriteDataFile.json", jsonData);       
-        return winPrizeData;
+    
     }
 
     public bool GetCanGivePrize(){
--- Assets/Scripts/Managers/SlotManager.cs
+++ Assets/Scripts/Managers/SlotManager.cs
@@ -7,20 +7,21 @@ using MonsterAR.Utility;
 public class SlotManager : Singleton<SlotManager>
 {
     // Start is called before the first frame update
-    [SerializeField] private Slot[] slots;
-    [SerializeField] private Sprite[] prizeSprites;
+    [SerializeField] private Slot[] slots = null;
+    [SerializeField] private Sprite[] itemSprites = null;
 
-    private int slotResultA;
-    private int slotResultB;
-    private int slotResultC;
+    private int[] slotResults = new int[3];
+    
 
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < slots.Length; i++)
         {
-            slots[i].transform.GetChild(0).GetChild(0).GetChild(i).GetChild(0).GetComponent<Image>().sprite = prizeSprites[i];            
+            slots[i].transform.GetChild(0).GetChild(0).GetChild(i).GetChild(0).GetComponent<Image>().sprite = itemSprites[i];            
         }
+
+        // AvoidWinResult();
     }
 
     IEnumerator RollingSlots(){
@@ -29,36 +30,109 @@ public class SlotManager : Singleton<SlotManager>
             slots[i].StartRolling();            
             yield return new WaitForSeconds(0.3f);   
         }
+        yield return new WaitForSeconds(2);   
+        
+        // if(PrizeManager.Instance.CheckCanGivePrizeByTime())
+            RandomResult();
+        // else
+            // AvoidWinResult();
     }
 
     public void GoRolling(){
-        StartCoroutine(RollingSlots());
-        RandomResult();
+        StartCoroutine(RollingSlots());        
+    }
+
+    public void SetSlotResults(int value){
+        slotResults[0] = value;
+        slotResults[1] = value;
+        slotResults[2] = value;
     }
 
     public void RandomResult(){
-        slotResultA = Random.Range(0, prizeSprites.Length);
-        slotResultB = Random.Range(0, prizeSprites.Length);
-        slotResultC = Random.Range(0, prizeSprites.Length);
-        Debug.Log(slotResultA + " " + slotResultB + " " + slotResultC);
+        Debug.Log("Random Result");
+
+        slotResults[0] = Random.Range(0, itemSprites.Length);
+        slotResults[1] = Random.Range(0, itemSprites.Length);
+        slotResults[2] = Random.Range(0, itemSprites.Length);
+
+        slotResults[0] = 0;
+        slotResults[1] = 0;
+        slotResults[2] = 0;
+
+        PrizeManager.Instance.CheckResult();
+
+        
     }
 
-    public Sprite GetPrizeSprite(int index){
-        return prizeSprites[index];
+    public void StopRollingSlots(){
+
+        //Stop Rolling Slots
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log("Result Slot " + i + ": " + slotResults[i]);
+            slots[i].StopRolling(i * 0.3f);
+        }
     }
 
-    public Sprite[] GetPrizeSprites(){
-        return prizeSprites;
+
+    
+
+    public void AvoidWinResult(){
+        Debug.Log("Avoiding Win Result");
+        int avoidSlot = Random.Range(0,3);
+
+        slotResults[0] = Random.Range(0, itemSprites.Length);
+        slotResults[1] = Random.Range(0, itemSprites.Length);
+        slotResults[2] = Random.Range(0, itemSprites.Length);
+
+        // slotResults[0] = 0;
+        // slotResults[1] = 0;
+        // slotResults[2] = 0;
+
+        while (slotResults[0] == slotResults[1] && slotResults[1] == slotResults[2])
+        {
+            Debug.Log("Result Same, Random Again");
+            slotResults[avoidSlot] = Random.Range(0, itemSprites.Length);
+        }
+        
+        PrizeManager.Instance.CheckResult();                
+    }
+
+    public void EmptyPrizeResult(){
+        Debug.Log("Avoiding Win Result");
+        int avoidSlot = Random.Range(0,3);
+
+        slotResults[0] = Random.Range(0, itemSprites.Length);
+        slotResults[1] = Random.Range(0, itemSprites.Length);
+        slotResults[2] = Random.Range(0, itemSprites.Length);
+
+        // slotResults[0] = 0;
+        // slotResults[1] = 0;
+        // slotResults[2] = 0;
+
+        while (slotResults[0] == slotResults[1] && slotResults[1] == slotResults[2])
+        {
+            Debug.Log("Result Same, Random Again");
+            slotResults[avoidSlot] = Random.Range(0, itemSprites.Length);
+        }
+    }
+
+    public Sprite GetItemSprite(int index){
+        return itemSprites[index];
+    }
+
+    public Sprite[] GetItemSprites(){
+        return itemSprites;
     }
 
     public int GetSlotResultA(){
-        return slotResultA;
+        return slotResults[0];
     }
     public int GetSlotResultB(){
-        return slotResultB;
+        return slotResults[1];
     }
     public int GetSlotResultC(){
-        return slotResultC;
+        return slotResults[2];
     }
 
    
